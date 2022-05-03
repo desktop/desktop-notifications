c5154d9990e46108718db4c0bbb0d2fddaaccb54
Sergio Padrino <sergio.padrino@gmail.com>
2022-05-03 16:02:57 +0200

    Export supportsNotifications

--- lib/index.ts
+++ lib/index.ts
@@ -5,4 +5,5 @@ export {
   terminateNotifications,
   getNotificationsPermission,
 } from './native-module'
+export { supportsNotifications } from './supports-notifications'
 export { onNotificationEvent } from './notification-callback'
