3a103d7ff6f3bbd86b28e6a384f0df2c36701931
Sergio Padrino <sergio.padrino@gmail.com>
2022-06-24 11:31:51 +0200

    Only support the W10 Creators Update and newer

--- lib/notification-support.ts
+++ lib/notification-support.ts
@@ -27,11 +27,19 @@ function supportsDarwinNotifications() {
 }
 
 function supportsWindowsNotifications() {
+  // The Windows Notification APIs we use are only available on Windows 10+, and
+  // some of them are limited before the Creators Update (build 15063).
+  const CreatorsUpdateBuildNumber = 15063
+
   const versionComponents = os.release().split('.')
   const majorVersion = parseInt(versionComponents[0], 10)
+  const buildNumber =
+    versionComponents.length >= 3
+      ? parseInt(versionComponents[2], 10)
+      : CreatorsUpdateBuildNumber
 
-  // Only Windows 10 and newer are supported
-  return majorVersion >= 10
+  // Only Windows 10 (15063) and newer are supported
+  return majorVersion >= 10 && buildNumber >= CreatorsUpdateBuildNumber
 }
 
 /**
